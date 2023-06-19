bf8ee812f0ba319f6389747ef897a4f9277aeac8
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-06-19 14:49:26 -0700

    Update  method

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,23 +6,18 @@ class RequestsController < ApplicationController
   def create
     @request = Request.new(request_params)
     if @request.save
-      # redirect to the request's show page
-    else
-      render 'new'
+      render json: @request, status: :created
+      else
+      render json: @request.errors, status: :unprocessable_entity
     end
-  end 
-  
-
-  def edit
-    @request = Request.find(params[:id])
   end
 
   def update
     @request = Request.find(params[:id])
     if @request.update(request_params)
-      # redirect to the request's show page
+      render json: @request, status: :ok
     else
-      render 'edit'
+      render json: @request.errors, status: :unprocessable_entity
     end
   end
 
@@ -44,7 +39,8 @@ class RequestsController < ApplicationController
 
     private
 
-  def request_params
-    params.require(:request).permit(:description, :status, :urgency_level, :expected_completion_date)
-  end
+    def request_params
+      params.require(:request).permit(:description, :status, :urgency_level, :expected_completion_date, :user_id, :department_id)
+    end
+    
 end
